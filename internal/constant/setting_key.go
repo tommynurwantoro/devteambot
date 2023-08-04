3c972880c99ffbe5959bdd6ff91393579c0134e3
Tommy Nurwantoro <tommy_nurwantoro@linkaja.id>
2023-08-04 20:38:47 +0700

    Remove unused redis key

--- internal/constant/setting_key.go
+++ internal/constant/setting_key.go
@@ -5,9 +5,6 @@ type SettingKeyType uint
 const (
 	Admin SettingKeyType = iota
 	SuperAdmin
-	CoinEmoji
-	LogCoinChannelID
-	ModReportChannelID
 )
 
 type SettingKey struct {
@@ -18,9 +15,6 @@ func NewSettingKey() SettingKey {
 	key := make(map[SettingKeyType]string)
 	key[Admin] = "admin"
 	key[SuperAdmin] = "super_admin"
-	key[CoinEmoji] = "coin_emoji"
-	key[LogCoinChannelID] = "log_coin_channel_id"
-	key[ModReportChannelID] = "mod_report_channel_id"
 
 	return SettingKey{key}
 }
@@ -33,16 +27,4 @@ func (c *SettingKey) Admin() string {
 
 func (c *SettingKey) SuperAdmin() string {
 	return c.Key[SuperAdmin]
-}
-
-func (c *SettingKey) CoinEmoji() string {
-	return c.Key[CoinEmoji]
-}
-
-func (c *SettingKey) LogCoinChannelID() string {
-	return c.Key[LogCoinChannelID]
-}
-
-func (c *SettingKey) ModReportChannelID() string {
-	return c.Key[ModReportChannelID]
 }
