3a74bb2a28b50ceb42bca85c5fdabc99dd9339fa
Tommy <tommy.nurwantoro@gmail.com>
2023-09-21 12:42:05 +0700

    Fix function scheduler

--- internal/adapter/scheduler/scheduler.go
+++ internal/adapter/scheduler/scheduler.go
@@ -38,11 +38,17 @@ func (s *Scheduler) Startup() error {
 		s.GetSholatSchedule(ctx)
 	})
 
-	scheduler.Every(1).Day().Monday().Tuesday().Wednesday().Thursday().Friday().At("07:55").Do(s.SendReminderPresensi, ctx)
+	scheduler.Every(1).Day().Monday().Tuesday().Wednesday().Thursday().Friday().At("07:55").Do(func() {
+		s.SendReminderPresensi(ctx)
+	})
 
-	scheduler.Every(1).Day().Monday().Tuesday().Wednesday().Thursday().Friday().At("17:05").Do(s.SendReminderPresensi, ctx)
+	scheduler.Every(1).Day().Monday().Tuesday().Wednesday().Thursday().Friday().At("17:05").Do(func() {
+		s.SendReminderPresensi(ctx)
+	})
 
-	scheduler.Every(1).Minute().Monday().Tuesday().Wednesday().Thursday().Friday().Do(s.SendReminderSholat, ctx)
+	scheduler.Every(1).Minute().Monday().Tuesday().Wednesday().Thursday().Friday().Do(func() {
+		s.SendReminderSholat(ctx)
+	})
 
 	scheduler.StartAsync()
 
